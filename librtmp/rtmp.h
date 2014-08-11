dc5f02d9bc3e6893ad011e38e4d0e91b523c20fc
Anthony Violo <anthony.violo@ubicast.eu>
2014-08-11 10:45:45 +0200

    * Added asynchronously option to avoid that send function could block. Thanks to nonblock send, the RTMPSockBuf_Send return could be check to know if all data was send before timeout (for example knowing if there is enough bandwith) * Added tcp-timeout option to avoid block connection function when no route is defined or when users have a bad internet connection

--- librtmp/rtmp.h
+++ librtmp/rtmp.h
@@ -132,6 +132,8 @@ extern "C"
     char sb_buf[RTMP_BUFFER_CACHE_SIZE];	/* data read from socket */
     int sb_timedout;
     void *sb_ssl;
+    int asynchronous;
+    unsigned long int tcp_timeout;
   } RTMPSockBuf;
 
   void RTMPPacket_Reset(RTMPPacket *p);
